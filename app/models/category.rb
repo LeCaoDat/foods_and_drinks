71cc141cddac0b8df39c6962f4b0d7e0fc7c3f74
LeCaoDat <hoangtuut1909@gmail.com>
2018-01-04 15:13:34 +0700

    Filter product by Alphabet, Categories, Price, Rating, Classify

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,14 +2,25 @@ class Category < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :suggests, dependent: :destroy
   has_many :childs, foreign_key: "parent_id", class_name: Category.name, dependent: :destroy
+  scope :root_categories, ->{where parent_id: Settings.categories.root_id}
 
-  def all_product
-    product_array = []
-    products.each{|e| product_array << e}
+  def find_childs_id_to_array
+    children_array = []
     childs.all.find_each do |child|
-      child.products.each{|e| product_array << e}
-      child.all_product
+      children_array << child.id
+      children_array << child.find_childs_id_to_array
     end
-    product_array
+    children_array.flatten
+  end
+
+  def all_children level = 0
+    children_array = []
+    level += 1
+    childs.all.find_each do |child|
+      child.name = I18n.t("categories.level") * level + child.name
+      children_array << child
+      children_array << child.all_children(level)
+    end
+    children_array.flatten
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,29 @@ class Product < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :ratings, dependent: :destroy
   mount_uploader :picture, PictureUploader
+  scope :filter_by_alphabet, ->(alpha){where("name LIKE ?", "#{alpha}%") if alpha.present?}
+  scope :filter_by_name, ->(name){where("name LIKE ?", "%#{name}%") if name.present?}
+  scope :filter_by_category, ->(category_id){where(category_id: category_id) if category_id.present?}
+  scope :filter_by_min_price, ->(min_price){where("price >= ?", min_price) if min_price.present?}
+  scope :filter_by_max_price, ->(max_price){where("price <= ?", max_price) if max_price.present?}
+  scope :filter_by_rate, (lambda do |rate|
+    select("products.*").joins("JOIN ratings r ON products.id = r.product_id")
+    .group(:id, :name, :price, :quality, :detail, :picture, :category_id, :created_at, :updated_at)
+    .having("ROUND(AVG(rate)) = ?", rate)
+  end)
 
   def average_rating
     (result = ratings.average :rate) ? result : 0
+  end
+
+  def self.filter_product params
+    result = Product.all
+    result = result.filter_by_alphabet params[:alpha] if params[:alpha].present?
+    result = result.filter_by_name params[:name] if params[:name].present?
+    result = result.filter_by_category params[:category_id] if params[:category_id].present?
+    result = result.filter_by_min_price params[:min_price] if params[:min_price].present?
+    result = result.filter_by_max_price params[:max_price] if params[:max_price].present?
+    result = result.filter_by_rate params[:rate] if params[:rate].present?
+    result
   end
 end
