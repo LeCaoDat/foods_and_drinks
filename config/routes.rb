814845854a71b61a769b33d9ac63d9710030d062
thaitran <thaitran.sts@gmail.com>
2018-01-06 12:36:55 +0700

    sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get "/login", to: "sessions#new"
   get "/signup", to: "users#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users, only: %i(new create show)
   resources :products
 end
